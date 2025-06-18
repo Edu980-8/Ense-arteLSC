725b65e7389d1d841ad6c3d53ba8ac1879fe111d
eduardortegon98 <eo@blurealty.ai>
2025-06-17 22:05:46 -0500

    Eliminar carpeta modelo y dejar de rastrearla en Git

--- modelo/metrics.py
+++
@@ -1,8 +0,0 @@
-# metrics.py
-from evaluate import load
-accuracy = load("accuracy")
-
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    preds = logits.argmax(-1)
-    return accuracy.compute(predictions=preds, references=labels)
